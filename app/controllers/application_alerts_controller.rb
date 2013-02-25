5683bb1ad925b5d54dfa53ec7b0b1cc2d9d2d710
shobhavenk <shobha.venkatesh@strata.co.in>
2013-02-25 18:56:38 +0530

    added build in applnalert

--- app/controllers/application_alerts_controller.rb
+++ app/controllers/application_alerts_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationAlertsController < ApplicationController
   respond_to :html, :xml, :json
   
+  def build_application_alert(user_application,application_alert_hash)
+    ApplicationAlert.build(user_application,application_alert_hash)
+  end
+
   def create
-    @alert = ApplicationAlert.new(params[:application_alert])
+    user_application = UserApplication.find_by_name(params[:application_name])
+    @alert = build_application_alert(user_application,params)
+    # @alert = ApplicationAlert.new(params[:application_alert])
     @error = {:error => 'creating alert error'}
     if @alert.save
       respond_to do |format|
--- app/models/application_alert.rb
+++ app/models/application_alert.rb
@@ -1,4 +1,21 @@
 class ApplicationAlert < ActiveRecord::Base
   attr_accessible :account_name, :alert_url, :application_name, :long_description, :message, :severity, :short_description, :user_application_id
   belongs_to :user_application
+
+  class << self
+    def build(user_application,options = {})
+      # attributes = {:user_application => user_application}
+      obj_params = {:account_name => options[:account_name],:application_name => options[:application_name], :alert_url => options[:alert_url], :long_description => options[:long_description], :message => options[:message],:severity => options[:severity], :short_description => options[:short_description]}
+      ApplicationAlert.new(obj_params).tap do |appln|
+        # appln.account_name = appln.account_name
+        # appln.alert_url = appln.alert_url
+        # appln.application_name = appln.application_name
+        # appln.long_description = appln.long_description
+        # appln.short_description = appln.short_description
+        # appln.message = appln.message
+        # appln.severity = appln.severity
+        # appln.user_application_id = appln.user_application.id
+      end
+    end
+  end
 end
