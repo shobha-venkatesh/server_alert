76eaf39a0f33a2ed6be4f689cbbfb120a376fed2
shobhavenk <shobha.venkatesh@strata.co.in>
2013-02-26 12:28:53 +0530

    small change in appln alert

--- app/models/application_alert.rb
+++ app/models/application_alert.rb
@@ -5,11 +5,12 @@ class ApplicationAlert < ActiveRecord::Base
   class << self
     def build(user_application,options = {})
       # attributes = {:user_application => user_application}
+      unless options.nil?
       p "options for Alert create ~ #{options.inspect}"
       obj_params = {:account_name => options["account_name"], :application_name => options["application_name"], 
                     :alert_url => options["alert_url"], :long_description => options["long_description"], 
                     :message => options["message"], :severity => options["severity"], 
-                    :short_description => options["short_description"], :user_application_id => 1 } unless options.nil?
+                    :short_description => options["short_description"], :user_application_id => 1 } 
       ApplicationAlert.new(obj_params).tap do |appln|
         # appln.account_name = appln.account_name
         # appln.alert_url = appln.alert_url
@@ -20,6 +21,7 @@ class ApplicationAlert < ActiveRecord::Base
         # appln.severity = appln.severity
         # appln.user_application_id = appln.user_application.id
       end
+    end
     end
   end
 end
