a551e7ab21a732d9be5c537e937c0b21370d9738
shobhavenk <shobha.venkatesh@strata.co.in>
2013-02-26 17:15:49 +0530

    added condition to check params

--- app/controllers/application_alerts_controller.rb
+++ app/controllers/application_alerts_controller.rb
@@ -5,16 +5,28 @@ class ApplicationAlertsController < ApplicationController
     ApplicationAlert.build(user_application,application_alert_hash)
   end
 
+  # def build_deployment_alert(deployment_alert_hash)
+  #   DeploymentAlert.build(deployment_alert_hash)
+  # end
+
+
   def create
     user_application = UserApplication.find_by_name(params[:application_name])
-    @alert = build_application_alert(user_application,params[:alert])
+    @alert = build_application_alert(user_application,params[:alert]) if params.has_key?("alert")
+    # @deploy_alert = build_deployment_alert(params[:deployment]) if params.has_key?("deployment")
     # @alert = ApplicationAlert.new(params[:application_alert])
-    @error = {:error => 'creating alert error'}
-    if @alert.save
+    @error = {:error => 'Error while creating record'}
+    
+    if !@alert.nil? && @alert.save
       respond_to do |format|
-            format.json { render :json => @alert}
-            format.xml { render :xml => @alert}
-        end
+            format.json { render :nothing => true, :status => :ok}
+            format.xml { render :nothing => true, :status => :ok }
+      end
+    # elsif @deploy_alert.save
+    #   respond_to do |format|
+    #         format.json { render :nothing => true, :status => :ok}
+    #         format.xml { render :nothing => true, :status => :ok }
+    #   end
     else
       respond_to do |format|
         format.json { render :json => @error}
