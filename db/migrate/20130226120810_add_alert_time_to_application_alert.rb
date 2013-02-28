73da67ea9e903b1fd86d9b8aae1b41786e5f7f1e
shobhavenk <shobha.venkatesh@strata.co.in>
2013-02-28 17:44:53 +0530

    change datatype

--- db/migrate/20130226120810_add_alert_time_to_application_alert.rb
+++ db/migrate/20130226120810_add_alert_time_to_application_alert.rb
@@ -1,5 +1,5 @@
 class AddAlertTimeToApplicationAlert < ActiveRecord::Migration
   def change
-    add_column :application_alerts, :alert_time, "timestamp with local time zone"
+    add_column :application_alerts, :alert_time, :timestamp
   end
 end
