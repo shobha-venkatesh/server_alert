4520f8a68111002acd83fd32cb6697c9622846cc
shobhavenk <shobha.venkatesh@strata.co.in>
2013-08-24 10:12:00 +0530

    added random string generation

--- app/admin/api_tokens.rb
+++ app/admin/api_tokens.rb
@@ -1,8 +1,19 @@
 ActiveAdmin.register ApiToken do
-  controller do
-     def build_api_token
- 	     user = current_user
- 	     ApiToken.build(user)
- 	  end
- 	end
+  member_action :create, :method => :post do
+  	user = 1
+    build_token = build(user)
+    redirect_to :action => :index, :notice => "Created!"
+  	end
+
+  	controller do
+  		private
+  		def build(user)
+  		    ApiToken.new do |api| 
+  			  api.token = SecureRandom.hex(5)
+  		 	  # later user id will be changed
+  		 	  api.user_id = user
+  		 	  api.save
+  	        end
+        end
+  	end
 end
--- app/helpers/api_tokens_helper.rb
+++ app/helpers/api_tokens_helper.rb
@@ -1,6 +1,2 @@
 module ApiTokensHelper
-	def build_api_token
- 	 user = current_user
- 	 ApiToken.build(user)
-  end
 end
