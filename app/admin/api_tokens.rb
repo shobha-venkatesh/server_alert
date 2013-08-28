83bbce9bb7a1ea9997c1e1c2a3abd0daf8bed40a
shobhavenk <shobha.venkatesh@strata.co.in>
2013-08-28 10:22:55 +0530

    added admin user_id

--- app/admin/api_tokens.rb
+++ app/admin/api_tokens.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register ApiToken do
   member_action :create, :method => :post do
-  	user = 1
+  	user = current_admin_user.id
     build_token = build(user)
     redirect_to :action => :index, :notice => "Created!"
   	end
@@ -10,7 +10,6 @@ ActiveAdmin.register ApiToken do
   		def build(user)
   		    ApiToken.new do |api| 
   			  api.token = SecureRandom.hex(5)
-  		 	  # later user id will be changed
   		 	  api.user_id = user
   		 	  api.save
   	        end
