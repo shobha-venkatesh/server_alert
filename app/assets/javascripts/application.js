f7fa3c15a7bfd7231346a0fe00f857071fefe22a
shobhavenk <shobha.venkatesh@strata.co.in>
2013-08-30 23:05:36 +0530

    commented timeliner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,30 +14,30 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
-$('.timeline_container').mousemove(function(e)
-{
-var topdiv=$("#containertop").height();
-var pag= e.pageY - topdiv-26;
-$('.plus').css({"top":pag +"px", "background":"url('images/plus.png')","margin-left":"1px"});}).mouseout(function()
-{
-$('.plus').css({"background":"url('')"});
-});
+// $('.timeline_container').mousemove(function(e)
+// {
+// var topdiv=$("#containertop").height();
+// var pag= e.pageY - topdiv-26;
+// $('.plus').css({"top":pag +"px", "background":"url('images/plus.png')","margin-left":"1px"});}).mouseout(function()
+// {
+// $('.plus').css({"background":"url('')"});
+// });
 
-function Arrow_Points()
-{ 
-var s = $('#container').find('.item');
-$.each(s,function(i,obj){
-var posLeft = $(obj).css("left");
-$(obj).addClass('borderclass');
-if(posLeft == "0px")
-{
-html = "<span class='rightCorner'></span>";
-$(obj).prepend(html); 
-}
-else
-{
-html = "<span class='leftCorner'></span>";
-$(obj).prepend(html);
-}
-});
-}
+// function Arrow_Points()
+// { 
+// var s = $('#container').find('.item');
+// $.each(s,function(i,obj){
+// var posLeft = $(obj).css("left");
+// $(obj).addClass('borderclass');
+// if(posLeft == "0px")
+// {
+// html = "<span class='rightCorner'></span>";
+// $(obj).prepend(html); 
+// }
+// else
+// {
+// html = "<span class='leftCorner'></span>";
+// $(obj).prepend(html);
+// }
+// });
+// }
